b01b2b6578047c483d26daa5f0f05fd1f0a9912c
ziayiing <ziayiing86@gmail.com>
2023-07-23 10:56:50 +0800

    imported weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./WeatherSearch";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Kota Kinsbalu" />
+        <footer>
+          This project was coded by{""}
+          <a
+            href="https://www.ziayiing86@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ling Zia Yiing
+          </a>
+          {""}
+          and is{""}
+          <a
+            href="https://github.com/fredacee/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
